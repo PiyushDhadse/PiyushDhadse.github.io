bff6fc9a20882a1f5a8dee22a1d2350726de56f0
Piyush Dhadse <dhadsepiyush54@gmail.com>
2025-12-11 01:21:04 +0530

    New updated portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Add this line
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
